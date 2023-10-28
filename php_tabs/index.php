0fe45407a34c0c5a951bba78956beef518922d72
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2023-10-29 01:48:58 +0800

    update-6

--- php_tabs/index.php
+++ php_tabs/index.php
@@ -16,12 +16,13 @@
     ?>
   </nav>
 
-  <section class="md:px-36 px-10 duration-300 z-0 grid lg:grid-cols-2 grid-cols-1 gap-9 border-red-300 border">
-    <img src=".././img/STIart.png" alt="art-img" class="m-auto h-auto w-auto">
+  <section class="md:px-36 px-10 duration-300 z-0 grid lg:grid-cols-2 grid-cols-1 gap-9 border">
+    <img src=".././img/STIart.png" alt="art-img" class="m-auto h-auto w-auto pb-10">
     <div class="m-auto tracking-wide">
-      <h1 class="md:text-4xl text-3xl text-center lg:text-start mb-5 font-black text-[#003568]">STI 😎 With Healthy
+      <h1 class="md:text-4xl text-3xl text-center lg:text-start mb-5 font-black text-[#003568] cursor-default">STI 😎
+        With Healthy
         Lifestyle</h1>
-      <p class="text-[#004e94] duration-300 text-justify md:text-start">A healthy and meaningful life requires an
+      <p class="text-[#004e94] duration-300 text-justify lg:text-start">A healthy and meaningful life requires an
         emotional state
         of well-being. It
         includes the
@@ -29,12 +30,68 @@
         comprehend, control, and express emotions in a way that has a positive influence on your general quality of
         life. This manual will examine the numerous facets of emotional health and will arm you with knowledge,
         techniques, and tools to support and improve your emotional health.</p>
-      <button
-        class="border-[#dba047] border-2 py-2 px-10 shadow-md tracking-wider rounded-2xl bg-[#febd5b] hover:bg-[#5495C9] font-semibold hover:text-white hover:border-[#2e5679] text-[#005BAB] transition duration-300 ease-in-out mt-5">
-        Learn More.
-      </button>
+      <div class="flex justify-center items-center lg:justify-start mt-5">
+        <button
+          class="border-[#dba047] border-2 py-2 px-10 shadow-md tracking-wider rounded-2xl bg-[#febd5b] hover:bg-[#5495C9] font-semibold hover:text-white hover:border-[#2e5679] text-[#005BAB] transition duration-300 ease-in-out">
+          Learn More.
+        </button>
+      </div>
     </div>
   </section>
+
+  <hr class="border border-b-slate-400 mt-10 mb-5">
+
+  <section class="md:px-36 px-10 duration-300 z-0 mt-20 border tracking-wide">
+    <h1 class="text-3xl font-extrabold text-[#003568] cursor-default">Welcome to the "Exploring Emotional Well-Being"
+    </h1>
+    <p class="text-[#004e94] text-lg mt-3">We are excited to have you participate in this important study focused on
+      understanding
+      emotions and
+      emotional
+      well-being.
+    </p>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 lg:grid-cols-4 mt-14">
+      <div class="relative overflow-hidden cursor-pointer group">
+        <img src=".././img/depression.jpg" alt="health" class="rounded-full object-cover h-72 w-72 mx-auto">
+        <div
+          class="absolute w-3/4 left-1/2 transform -translate-x-1/2 text-center bg-primary-color-dark bg-opacity-80 backdrop-filter backdrop-blur-lg border border-primary-color rounded-lg text-extra-light group-hover:-bottom-6 transition-all duration-300 ease-out text-white group-hover:-translate-y-[135px]">
+          <p class="text-sm my-1">Raise Awareness</p>
+          <p class="font-semibold text-base mb-2">Depression</p>
+        </div>
+      </div>
+
+      <div class="relative overflow-hidden cursor-pointer group">
+        <img src=".././img/adhd.jpg" alt="health" class="rounded-full object-cover h-72 w-72 mx-auto">
+        <div
+          class="absolute w-3/4 left-1/2 transform -translate-x-1/2 text-center bg-primary-color-dark bg-opacity-80 backdrop-filter backdrop-blur-lg border border-primary-color rounded-lg text-extra-light group-hover:-bottom-6 transition-all duration-300 ease-out text-white group-hover:-translate-y-[135px]">
+          <p class="text-sm my-1">Raise Awareness</p>
+          <p class="font-semibold text-base mb-2">ADHD</p>
+        </div>
+      </div>
+
+      <div class="relative overflow-hidden cursor-pointer group">
+        <img src=".././img/anxiety.jpg" alt="health" class="rounded-full object-cover h-72 w-72 mx-auto">
+        <div
+          class="absolute w-3/4 left-1/2 transform -translate-x-1/2 text-center bg-primary-color-dark bg-opacity-80 backdrop-filter backdrop-blur-lg border border-primary-color rounded-lg text-extra-light group-hover:-bottom-6 transition-all duration-300 ease-out text-white group-hover:-translate-y-[135px]">
+          <p class="text-sm my-1">Raise Awareness</p>
+          <p class="font-semibold text-base mb-2">Anxiety</p>
+        </div>
+      </div>
+
+      <div class="relative overflow-hidden cursor-pointer group">
+        <img src=".././img/bipolar.jpg" alt="health" class="rounded-full object-cover h-72 w-72 mx-auto">
+        <div
+          class="absolute w-3/4 left-1/2 transform -translate-x-1/2 text-center bg-primary-color-dark bg-opacity-80 backdrop-filter backdrop-blur-lg border border-primary-color rounded-lg text-extra-light group-hover:-bottom-6 transition-all duration-300 ease-out text-white group-hover:-translate-y-[135px]">
+          <p class="text-sm my-1">Raise Awareness</p>
+          <p class="font-semibold text-base mb-2">Bipolar Disorder</p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <hr class="border border-b-slate-400 mt-32 mb-5">
 
   <section>
 
